3bdf00c38aa661f232658d233d21f062183d425f
Anton Fediushin <imantonmeep@gmail.com>
2022-03-19 20:34:45 +0100

    Implement transform function

--- src/sha1_generic.adb
+++ src/sha1_generic.adb
@@ -49,8 +49,75 @@ package body SHA1_Generic is
    end Hash;
 
    procedure Transform (Ctx : in out Context; Buffer : Block) is
+      type Words is array (Natural range <>) of Unsigned_32;
+
+      Buffer_Words : Words (0 .. 15);
+      for Buffer_Words'Address use Buffer'Address;
+
+      W : Words (0 .. 79);
+
+      A         : Unsigned_32 := Ctx.H0;
+      B         : Unsigned_32 := Ctx.H1;
+      C         : Unsigned_32 := Ctx.H2;
+      D         : Unsigned_32 := Ctx.H3;
+      E         : Unsigned_32 := Ctx.H4;
+      Temporary : Unsigned_32;
    begin
-      null;
+      --  Byte order fix here
+
+      W (0 .. 15) := Buffer_Words;
+
+      for I in 16 .. 79 loop
+         W (I) :=
+           Rotate_Left
+             ((W (I - 3) xor W (I - 8) xor W (I - 14) xor W (I - 16)), 1);
+      end loop;
+
+      for I in 0 .. 19 loop
+         Temporary :=
+           Rotate_Left (A, 5) + Ch (B, C, D) + E + 16#5A82_7999# + W (I);
+         E := D;
+         D := C;
+         C := Rotate_Left (B, 30);
+         B := A;
+         A := Temporary;
+      end loop;
+
+      for I in 20 .. 39 loop
+         Temporary :=
+           Rotate_Left (A, 5) + Parity (B, C, D) + E + 16#6ED9_EBA1# + W (I);
+         E := D;
+         D := C;
+         C := Rotate_Left (B, 30);
+         B := A;
+         A := Temporary;
+      end loop;
+
+      for I in 40 .. 59 loop
+         Temporary :=
+           Rotate_Left (A, 5) + Maj (B, C, D) + E + 16#8F1B_BCDC# + W (I);
+         E := D;
+         D := C;
+         C := Rotate_Left (B, 30);
+         B := A;
+         A := Temporary;
+      end loop;
+
+      for I in 60 .. 79 loop
+         Temporary :=
+           Rotate_Left (A, 5) + Parity (B, C, D) + E + 16#CA62_C1D6# + W (I);
+         E := D;
+         D := C;
+         C := Rotate_Left (B, 30);
+         B := A;
+         A := Temporary;
+      end loop;
+
+      Ctx.H0 := Ctx.H0 + A;
+      Ctx.H1 := Ctx.H1 + B;
+      Ctx.H2 := Ctx.H2 + C;
+      Ctx.H3 := Ctx.H3 + D;
+      Ctx.H4 := Ctx.H4 + E;
    end Transform;
 
    function Ch (X, Y, Z : Unsigned_32) return Unsigned_32 is
