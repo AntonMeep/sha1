2aba4b22cdefc4dc31c23c42e0598a7880729711
Anton Fediushin <imantonmeep@gmail.com>
2022-03-22 15:23:48 +0100

    Add documentation

--- src/sha1.ads
+++ src/sha1.ads
@@ -3,5 +3,15 @@ with Ada.Streams; use Ada.Streams;
 with SHA1_Generic;
 
 package SHA1 is new SHA1_Generic
-  (Element => Stream_Element, Index => Stream_Element_Offset,
+  (Element       => Stream_Element, Index => Stream_Element_Offset,
    Element_Array => Stream_Element_Array);
+--  @summary
+--  Secure Hash Algorithm 1 in Ada, Ada.Streams interface
+--
+--  @description
+--  This package contains an instantation of SHA1_Generic package with the
+--  types from Ada.Streams package. This should be your default choice for
+--  most applications, but you can always use the generic package in case your
+--  program already defines own type representing a byte array.
+--
+--  See SHA1_Generic for more details
--- src/sha1_generic.ads
+++ src/sha1_generic.ads
@@ -2,30 +2,74 @@ with Interfaces;
 
 generic
    type Element is mod <>;
+   --  Element represents one byte of data
+
    type Index is range <>;
+   --  Index type of the Element_Array
+
    type Element_Array is array (Index range <>) of Element;
+   --  An array of bytes
 package SHA1_Generic with
    Pure,
    Preelaborate
 is
+   --  @summary
+   --  Generic Secure Hash Algorithm 1 implementation in Ada
+   --
+   --  @description
+   --  This package provides implementation of SHA1 algorithm and operates on
+   --  a generic Element_Array type, which represents an array of bytes.
+
+   pragma Compile_Time_Error
+     (Element'Modulus /= 256,
+      "'Element' type must be mod 2**8, i.e. represent a byte");
+
    Digest_Length : constant Index := 20;
-   Block_Length  : constant Index := 64;
+   --  Length (in bytes) of the hash result
+
+   Block_Length : constant Index := 64;
+   --  Block length (in bytes), not very useful for the end user
 
    subtype Digest is Element_Array (0 .. Digest_Length - 1);
+   --  Type representing the result of a hash function
 
    type Context is private;
+   --  Algorithm context, holds all of the necessary internal data. Always
+   --  initialized with correct data, calling Initialize() functions is not
+   --  required but is strongly advised.
 
    function Initialize return Context;
+   --  Initialize a Context properly. By default Context is already holding
+   --  all of the necessary initialization values, this function is provided
+   --  mainly for compatibility with other implementations.
+   --  @return An initialized context
+
    procedure Initialize (Ctx : out Context);
+   --  Ditto, but as a procedure.
 
    procedure Update (Ctx : in out Context; Input : String);
+   --  Update Ctx with data from Input
+
    procedure Update (Ctx : in out Context; Input : Element_Array);
+   --  Update Ctx with data from Input
 
    function Finalize (Ctx : in out Context) return Digest;
+   --  Compute hash value and return it.
+   --
+   --  Note: This modifies input Ctx, if you want to continue adding data to it
+   --  after calling Finalize, you should make a copy of it.
+   --  @return Hash of the data that was fed into Ctx
+
    procedure Finalize (Ctx : in out Context; Output : out Digest);
+   --  Ditto, but as a procedure.
 
    function Hash (Input : String) return Digest;
+   --  Compute hash of Input and return it. Essentially is an equivalent of
+   --  Initialize, Update(Input) and Finalize.
+
    function Hash (Input : Element_Array) return Digest;
+   --  Compute hash of Input and return it. Essentially is an equivalent of
+   --  Initialize, Update(Input) and Finalize
 private
    use Interfaces;
 
